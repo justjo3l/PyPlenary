da51b6ee678fc32d193e40d4337a550396e337ee
RunasSudo <runassudo@yingtongli.me>
2021-03-17 17:56:02 +1100

    Formatting adjustments

--- pyplenary/councilApp/templatetags/markdown.py
+++ pyplenary/councilApp/templatetags/markdown.py
@@ -7,8 +7,10 @@ register = template.Library()
 
 @register.filter(name='markdown')
 def filter_markdown(value):
-    return mark_safe(markdown.markdown(value))
+    # FIXME: Make this not cursed
+    return mark_safe(markdown.markdown(value).replace('<a ', '<a target="_blank" '))
 
 @register.filter
 def nopar(value):
+    # FIXME: Make this not cursed
     return mark_safe(value.replace('<p>', '').replace('</p>', ''))
