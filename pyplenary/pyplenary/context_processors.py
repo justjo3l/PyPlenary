ba8cabc5497f3610cc9ca87cce515cffdc2936f1
RunasSudo <runassudo@yingtongli.me>
2021-10-02 15:12:41 +1000

    Make agenda timezone configurable

--- pyplenary/pyplenary/context_processors.py
+++ pyplenary/pyplenary/context_processors.py
@@ -4,5 +4,6 @@ def pyplenary_settings(request):
     return {
     	'NAVBAR_NAME': settings.PYPLENARY_NAVBAR_NAME,
         'SITE_NAME': settings.PYPLENARY_SITE_NAME,
-        'SITE_TAGLINE': settings.PYPLENARY_SITE_TAGLINE
+        'SITE_TAGLINE': settings.PYPLENARY_SITE_TAGLINE,
+        'PYPLENARY_TZ': settings.PYPLENARY_TZ,
     }
