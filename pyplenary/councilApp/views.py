139a82ab644dd638ab72e261f2ced8baba851f5c
AllenZPGu <zhepu.gu@gmail.com>
2021-10-08 22:54:44 +1100

    update delegates list to correct for reps while not logged in

--- pyplenary/councilApp/views.py
+++ pyplenary/councilApp/views.py
@@ -133,7 +133,8 @@ def delegates(request):
             + list(Delegate.objects.exclude(authClone=request.user).exclude(speakerNum=0).filter(rep=True).order_by('speakerNum'))
             + list(Delegate.objects.exclude(authClone=request.user).exclude(speakerNum=0).filter(rep=False).order_by('speakerNum'))
     else:
-        allDelegates = Delegate.objects.exclude(speakerNum=0).order_by('speakerNum')
+        allDelegates = list(Delegate.objects.exclude(speakerNum=0)..filter(rep=True).order_by('speakerNum'))
+            + list(Delegate.objects.exclude(speakerNum=0).filter(rep=False).order_by('speakerNum'))
     return render(request, 'councilApp/delegates.html', {'allDelegates':allDelegates, 'active_tab':'delegates'})
 
 @login_required
